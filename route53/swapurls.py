f6dbdcf7ee7349d8d8d57cc05f22bd37afa77edd
Chakrabarti, Sandipan <SANDIPAN.CHAKRABARTI@gmail.com>
2017-09-10 20:05:15 -0700

    route53 utility

--- route53/swapurls.py
+++ route53/swapurls.py
@@ -17,18 +17,64 @@ def getHostedZones(dnsName, nextHostedZoneId):
             MaxItems='10',
         ) 
         
-        
-        
-        
-if __name__=="__main__":
+def getRecordSets(startRecordName, startRecordType, hostedZoneId):
+    if startRecordName is None:
+        return route53client.list_resource_record_sets(
+            HostedZoneId=hostedZoneId,
+            MaxItems='10'
+        ) 
+    else:
+        return route53client.list_resource_record_sets(
+            HostedZoneId=hostedZoneId,
+            StartRecordName = startRecordName,
+            StartRecordType = startRecordType,
+            MaxItems='10'
+        ) 
+                
+def getChangedRecordSet(name, rectype, ttl, newvalue) :
+    changeBatch = {}
+    changeBatch.update({'Comment': 'Updated'})
+    changes = []
+    change = {}
+    resourceRecordSet = {}
+    resourceRecordSet.update({'Name': name})
+    resourceRecordSet.update({'Type': rectype})
+    resourceRecordSet.update({'TTL': ttl})
+    resourceRecords = []
+    resourceRecord = {'Value':newvalue}
+    resourceRecords.append(resourceRecord)
+    resourceRecordSet.update({'ResourceRecords':resourceRecords})
+    change.update({'Action': 'UPSERT'})
+    change.update({'ResourceRecordSet': resourceRecordSet})
+    changes.append(change)
+    changeBatch.update({'Changes':changes})
+    return changeBatch
+
+def findRecordSet(recordSetUrl, recordSetType):
+    isTruncated = True;
+    nextRecordName = None
+    while(isTruncated):
+        response = getRecordSets(nextRecordName, recordSetType, hostedZoneId)
+        isTruncated = response['IsTruncated']
+        if 'NextRecordName' in response:
+            nextRecordName = response['NextRecordName']
+
+        resourceRecords = response['ResourceRecordSets']
+        for resourceRecord in resourceRecords: 
+            if resourceRecord['Name'] == recordSetUrl + '.' and resourceRecord['Type'] == recordSetType:
+                    return resourceRecord 
+
+    return None 
+
+def getHostedZoneId(hostedzonename): 
     istruncated = True
     nextHostedZoneId = None
     nextDNSName = None
-    zones = {}
     while(istruncated):
         response = getHostedZones(nextDNSName, nextHostedZoneId)
         istruncated = response['IsTruncated']
         hostedzones = response['HostedZones']
+        
         if 'NextHostedZoneId' in response:
             nextHostedZoneId = response['NextHostedZoneId']
         
@@ -36,11 +82,39 @@ if __name__=="__main__":
             nextDNSName = response['NextDNSName']
             
         for hostedzone in hostedzones:
-            zones.update({hostedzone['Name']: hostedzone['Id']})
+            if hostedzone['Name'] == hostedzonename:
+                return hostedzone['Id']
     
+    return None    
+
+        
+if __name__=="__main__":
+    hostedzoneName = 'asurionpa.com.'
+    recordSetId = 'hello-svc-memories.asurionpa.com'
+    recordSetType = 'CNAME'
+    newUrl = 'helloworld-3-alb-1052942687.us-east-1.elb.amazonaws.com'
     
-    response = route53client.list_resource_record_sets(
-        HostedZoneId=zones.get('asurionpa.com.')
-    )
+    hostedZoneId=getHostedZoneId(hostedzoneName)
+    recordset = findRecordSet(recordSetId, recordSetType)
     
-    print(response)
+    if recordset is None:
+        raise Exception('RecordSet Not found')
+    
+    name = recordset['Name'] 
+    rectype = recordset['Type']
+    ttl = recordset['TTL']
+    changeBatch = getChangedRecordSet(name, rectype, ttl, newUrl)
+    print(recordset) 
+    
+    try: 
+            response = route53client.change_resource_record_sets(
+                HostedZoneId=hostedZoneId,
+                ChangeBatch=changeBatch
+            )
+            if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                print('Route 53 Update request submitted successfully')
+            print("Record set updated to point to : " + newUrl)
+    except Exception as e:
+        print( e)
+     
+    
