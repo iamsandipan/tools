078cb80ecc6cffe6f08152604112d1fc27117366
Chakrabarti, Sandipan <SANDIPAN.CHAKRABARTI@gmail.com>
2017-09-12 16:08:36 -0700

    no message

--- mixpanel/businessmetrics.py
+++ mixpanel/businessmetrics.py
@@ -7,13 +7,10 @@ import mixpanel
 from mixpanel import Mixpanel
 mp = Mixpanel('664ffe7a8bdf85207bda500ac4251485')
 
-# Tracks an event, 'Sent Message',
-# with distinct_id user_id
-mp.track('11111111111111111111111', 'Sent Message')
+
 
 # You can also include properties to describe
 # the circumstances of the event
-mp.track('11111111111111111111111', 'Plan Upgraded', {
-    'Old Plan': 'Business',
-    'New Plan': 'Premium'
+mp.track('11111111111111111111111', 'Memories_OperationalMetrics', {
+    'TotalFiles': '100'
 })
